1bd8886883e7381e031daa11f6af5da3d3ce72c9
unknown <kysto27@gmail.com>
2023-12-23 22:36:20 +0300

    test db.connection

--- bot/connection/db.connection.js
+++ bot/connection/db.connection.js
@@ -1,16 +1,30 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-   process.env.POSTGRES_DATABASE,
-   process.env.POSTGRES_USERNAME,
-   process.env.POSTGRES_PASSWORD,
-   {
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      dialect: process.env.POSTGRES_DIALECT
-   }
-);
+// const db = new Sequelize(
+//    process.env.POSTGRES_DATABASE,
+//    process.env.POSTGRES_USERNAME,
+//    process.env.POSTGRES_PASSWORD,
+//    {
+//       host: process.env.POSTGRES_HOST,
+//       port: Number(process.env.POSTGRES_PORT),
+//       dialect: process.env.POSTGRES_DIALECT
+//    }
+// );
+
+const dbName = 'bd_neboprognozbot';
+const dbUser = 'kysto27';
+const dbPassword = 'B#5yBYUU%lPp';
+const dbHost = 'localhost';
+const dbPort = 5432;
+const dbDialect = 'postgres';
+
+const db = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    port: dbPort,
+    dialect: dbDialect
+});
+
 
 console.log(process.env.POSTGRES_DATABASE);
 console.log(process.env.POSTGRES_USERNAME);
