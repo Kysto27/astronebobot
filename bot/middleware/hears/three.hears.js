65a374fce60c5fa5c547b06455e49d306046fc74
unknown <kysto27@gmail.com>
2024-01-20 19:48:47 +0300

    fix

--- bot/middleware/hears/three.hears.js
+++ bot/middleware/hears/three.hears.js
@@ -1,4 +1,4 @@
 const bot = require('../../connection/token.connection');
 
-module.exports = bot.hears('Гороскоп', async (ctx) => ctx.scene.enter('threeWizard'));
+module.exports = bot.hears('Гороскопы', async (ctx) => ctx.scene.enter('threeWizard'));
 bot.command('horoscope', async (ctx) => ctx.scene.enter('threeWizard'));
--- bot/middleware/src/getWeeklyHoroscope.js
+++ bot/middleware/src/getWeeklyHoroscope.js
@@ -98,7 +98,7 @@ async function processHoroscopeData(url, Model, zodiacSigns) {
   }
 }
 const scheduleHoroscopeUpdates = () => {
-  cron.schedule('15 19 * * 6', () => {
+  cron.schedule('55 19 * * 6', () => {
     console.log('Запуск задачи по сбору данных гороскопа');
     fetchAndSaveHoroscope();
   });
